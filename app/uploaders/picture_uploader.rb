02497490296d28bf20f3d0076789ac24e950a3f7
Johan <info@johanvanderklift.nl>
2018-09-15 11:55:00 +0000

    fix app crash in heroku

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,7 +6,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if rails.env.production?
+  if Rails.env.production?
     storage :fog
   else
     storage :file
